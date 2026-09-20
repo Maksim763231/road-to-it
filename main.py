d43d5972275735edc8c8f9a99cdb00fd8f23f3ac
Maksim763231 <kashinmaksim2007@yandex.ru>
2026-09-20 21:15:16 +0300

    Запустил своего первого Telegram-бота

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+print(TOKEN is not None)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Привет! Я твой личный Telegram-бот 🚀")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
